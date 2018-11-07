0360d3d379e8d481a2354434ebb78bcaebb96715
mahaker <hideaki.matsunami@esm.co.jp>
2018-11-07 19:34:07 +0900

    rename SampleApplicationTest

--- spring-boot/src/test/java/com/mahaker/sample/SampleApplicationTest.java
+++ spring-boot/src/test/java/com/mahaker/sample/SampleApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SampleApplicationTests {
+public class SampleApplicationTest {
 
 	@Test
 	public void contextLoads() {
