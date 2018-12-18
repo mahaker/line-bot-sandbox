7633614e9ba0af33f6fedef2bd206e13faf8f53d
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2018-12-18 13:18:35 +0900

    TODO

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -2,19 +2,23 @@ import Express, { Request, Response } from 'express';
 import { 
     Client, middleware, 
     ClientConfig, MiddlewareConfig, 
-    WebhookEvent, MessageEvent, TemplateMessage, TemplateConfirm, TextMessage, Action, TextEventMessage} from '@line/bot-sdk';
+    WebhookEvent, MessageEvent, PostbackEvent,
+    TemplateMessage, TemplateConfirm, TextMessage, Action, TextEventMessage} from '@line/bot-sdk';
 
+// TODO 環境変数で指定したい。
 const clientConfig: ClientConfig = {
     channelAccessToken: 'ARwyenJOtWdAY/mKwItsp2eVHc5DLkBxUashhLOeRdkwQBTooRuMu+EBckCkRTZ8xWM30x3/U7TSUgqHZ3YO+RicTcBPoos/OKSAHBQzzxpzxRVZ03lddNJ1viCqq0G77N9CRZbYm62wPnO7YbNpCgdB04t89/1O/w1cDnyilFU=',
 };
 const middlewareConfig: MiddlewareConfig = {
     channelSecret: 'ef08dab5f407bd5e324c989267c8ecc1',
 };
+
 const botClient = new Client(clientConfig);
 const botMiddleware = middleware(middlewareConfig);
 
 const app = Express();
 
+// 不要なコントローラー（サーバー起動の動作確認のため）
 app.get('/', (request: Request, response: Response) => {
     return response.send('Hello mahaker!!');
 });
@@ -29,7 +33,7 @@ app.post('/webhook', botMiddleware, (request: Request, response: Response) => {
         });
 });
 
-function handleEvent(event: WebhookEvent): Promise<any> {
+function handleEvent(event: MessageEvent | PostbackEvent): Promise<any> {
     const userId: string | undefined = event.source.userId;
 
     if(event.type === 'postback') {
@@ -39,6 +43,7 @@ function handleEvent(event: WebhookEvent): Promise<any> {
         }
         return !!userId? botClient.pushMessage(userId, textMessage) : Promise.resolve(null);
     } else {
+        // TODO なんとかしたい
         const e: MessageEvent = event as MessageEvent;
         const m: TextEventMessage = e.message as TextEventMessage;
         const messageActions: Action[] = [
@@ -73,6 +78,7 @@ function handleEvent(event: WebhookEvent): Promise<any> {
             text: `${m.text}クイズ！`
         }
 
+        // TODO ifでまとめる
         !!userId? botClient.pushMessage(userId, textMessage) : Promise.resolve(null);
         return !!userId? botClient.pushMessage(userId, message) : Promise.resolve(null);
     }
